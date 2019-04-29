a79b7a523a41210616f5e50dabf0a1da2036ff35
Troy Wiegand <troy@troywiegand.com>
2019-04-29 09:34:46 -0400

    Tweak openCW script to be more powerful

--- scripts/openCW.sh
+++ scripts/openCW.sh
@@ -1,1 +1,2 @@
-echo "Legends\nArrow\nFlash\nSupergirl" | dmenu | xargs -I {} echo "Videos/CW/{}" | tee /tmp/show.txt  | xargs -I {} ls -R /home/troy/{} | grep .mp4 | dmenu -i | rev | cut -f2- | rev >> /tmp/show.txt; head -1 /tmp/show.txt | xargs -I {} echo "/home/troy/{}/`tail -1 /tmp/show.txt`" | xargs -I {} mpv {}
+ls Videos/CW/ | dmenu -i -l 10 | xargs -I {} echo "Videos/CW/{}" | tee /tmp/show.txt  | xargs -I {} ls -tR /home/troy/{} | grep .mp4 | rev | cut -c 5- | rev |  sed -e 's,_, ,g'| dmenu -i -l 10| sed -e 's, ,_,g' >> /tmp/show.txt;
+head -1 /tmp/show.txt | xargs -I {} echo "/home/troy/{}/`tail -1 /tmp/show.txt`" | xargs -I {} mpv {}.mp4
