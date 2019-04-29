1666ca2d98d7409bd983e1cbdb90eccd10dc2eac
Troy Wiegand <troy@troywiegand.com>
2019-04-29 13:20:38 -0400

    Add prompt on dmenu and make Shows appear in order of most recently downloaded.

--- scripts/openCW.sh
+++ scripts/openCW.sh
@@ -1,2 +1,2 @@
-ls Videos/CW/ | dmenu -i -l 10 | xargs -I {} echo "Videos/CW/{}" | tee /tmp/show.txt  | xargs -I {} ls -tR /home/troy/{} | grep .mp4 | rev | cut -c 5- | rev |  sed -e 's,_, ,g'| dmenu -i -l 10| sed -e 's, ,_,g' >> /tmp/show.txt;
+ls -t Videos/CW/ | dmenu -i -l 10 -p "What show?" | xargs -I {} echo "Videos/CW/{}" | tee /tmp/show.txt  | xargs -I {} ls -tR /home/troy/{} | grep .mp4 | rev | cut -c 5- | rev |  sed -e 's,_, ,g'| dmenu -i -l 10 -p "What episode?" | sed -e 's, ,_,g' >> /tmp/show.txt;
 head -1 /tmp/show.txt | xargs -I {} echo "/home/troy/{}/`tail -1 /tmp/show.txt`" | xargs -I {} mpv {}.mp4
