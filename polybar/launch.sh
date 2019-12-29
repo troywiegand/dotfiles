e2d56ae0a28e45a50f2647303b864ba79f9a8767
Troy Wiegand <troy@troywiegand.com>
2019-12-29 18:40:58 -0500

    Add polybar config

---
+++ polybar/launch.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+HDMI = $(xrandr -q | grep HDMI-1 | awk '{print $2}')
+DVI = $(xrandr -q | grep DVI-I-1-1 | awk '{print $2}')
+
+## Just one thing connected
+if [ $HDMI = "connected" ]; then
+	polybar -c /home/troy/.config/polybar/config.ini eDP-1 &
+	polybar HDMI-1 &
+## The dock thingy I have at runs it with these DVI 
+elif [ $DVI = "connected" ]; then
+	polybar -c /home/troy/.config/polybar/config.ini DVI-I-1-1 &
+	polybar -c /home/troy/.config/polybar/config.ini eDP-1 &
+	polybar -c /home/troy/.config/polybar/config.ini DVI-I-2-2 &
+else
+	polybar -c /home/troy/.config/polybar/config.ini eDP-1 &
+fi
+
