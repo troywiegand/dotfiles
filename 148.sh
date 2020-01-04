45517ecccaa80ab77336695e46a86218454d0ee8
Troy Wiegand <troy@troywiegand.com>
2020-01-04 10:53:41 -0500

    Add scripts to switch layouts

---
+++ 148.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+xrandr --output HDMI-2 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output eDP-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-2 --off
+
+for x in `seq 6 10`;
+do 
+	bspc desktop $x -m HDMI-1
+done
+
+bspc wm -r
---
+++ laptop.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+xrandr --output DVI-I-1-1 --off --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-I-2-2 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off
+
+bspc wm -r
